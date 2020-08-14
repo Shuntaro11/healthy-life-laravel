4a83a2e0cbe3811b5649a82973da520cec5fdd0b
shuntaro <shun.sea.f@gmail.com>
2020-08-14 19:28:00 +0900

    scss修正

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE HTML>
+<html lang="ja">
 <html>
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Healthy Life</title>
-    <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
