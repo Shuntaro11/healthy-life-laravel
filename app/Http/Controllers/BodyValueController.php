b74b285bdb7d4cbbffd010eddc8a3691170d2b7a
shuntaro <shun.sea.f@gmail.com>
2020-08-24 23:26:09 +0900

    Update BodyValueController.php

--- app/Http/Controllers/BodyValueController.php
+++ app/Http/Controllers/BodyValueController.php
@@ -17,30 +17,28 @@ class BodyValueController extends Controller
         ]);
 
         $auth = Auth::user();
-        $before_body_values = BodyValue::where('user_id', "{$auth->id}")->where('date', $request->date)->get();
+        $before_body_values = BodyValue::where('user_id', "{$auth->id}")->where('date', $request->date)->first();
         
-
         $bmi = round($request->weight / (( $request->height / 100 ) * ( $request->height / 100 )), 1);
         
         if(isset($before_body_values)){
 
-            $body_values == $before_body_values;
+            $body_values = $before_body_values;
 
         } else {
 
             $body_values = new BodyValue;
+            $body_values->user_id = Auth::user()->id;
+            $body_values->date = $request->date;
 
         }
 
-        $body_values->date = $request->date;
         $body_values->weight = $request->weight;
         $body_values->height = $request->height;
         $body_values->bmi = $bmi;
-        $body_values->user_id = Auth::user()->id;
         
         $body_values->save();
 
-        $auth = Auth::user();
         $body_values = $auth->body_values()->latest()->take(14)->get();
 
         $days = array();
